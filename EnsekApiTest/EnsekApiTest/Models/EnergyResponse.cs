0521cbbc9305de1be94ab59611938a78b8f60445
harrolg <99892115+harrolg@users.noreply.github.com>
2023-06-04 19:33:04 +0100

    Added automation project

---
+++ EnsekApiTest/EnsekApiTest/Models/EnergyResponse.cs
@@ -0,0 +1,42 @@
+﻿namespace EnsekApiTest.Models
+{
+    public class EnergyResponse
+    {
+        public Electric electric { get; set; }
+        public Gas gas { get; set; }
+        public Nuclear nuclear { get; set; }
+        public Oil oil { get; set; }
+    }
+
+    public class Electric
+    {
+        public int energy_id { get; set; }
+        public float price_per_unit { get; set; }
+        public int quantity_of_units { get; set; }
+        public string unit_type { get; set; }
+    }
+
+    public class Gas
+    {
+        public int energy_id { get; set; }
+        public float price_per_unit { get; set; }
+        public int quantity_of_units { get; set; }
+        public string unit_type { get; set; }
+    }
+
+    public class Nuclear
+    {
+        public int energy_id { get; set; }
+        public float price_per_unit { get; set; }
+        public int quantity_of_units { get; set; }
+        public string unit_type { get; set; }
+    }
+
+    public class Oil
+    {
+        public int energy_id { get; set; }
+        public float price_per_unit { get; set; }
+        public int quantity_of_units { get; set; }
+        public string unit_type { get; set; }
+    } 
+}
---
+++ EnsekApiTest/EnsekApiTest/Models/OrdersResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace EnsekApiTest.Models
+{
+    public class OrdersResponse
+    {
+        public List<Order> Orders { get; set; }
+    }
+
+    public class Order
+    {
+        public string fuel { get; set; }
+        public string id { get; set; }
+        public int quantity { get; set; }
+        public string time { get; set; }
+    }
+
+    
+}
+
+
